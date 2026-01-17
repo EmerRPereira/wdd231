1941d67b82bd5454478f99dfc69cc086d56570bd
EmerRPereira <104523253+EmerRPereira@users.noreply.github.com>
2026-01-17 17:12:56 -0300

    Add date.js to update current year and last modified date

---
+++ chamber/scripts/date.js
@@ -0,0 +1,5 @@
+// Atualizar ano atual
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Atualizar data da última modificação
+document.getElementById("lastModified").textContent += document.lastModified;
