8edb08b97961efdb9a789a59508d00ff85b53bfa
EmerRPereira <104523253+EmerRPereira@users.noreply.github.com>
2026-01-30 10:29:33 -0300

    Update date.js

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,3 +1,10 @@
+/*****************************************************
+ * scripts/date.js
+ * WDD231 – Chamber Home Page
+ * Date and Last Modified Script
+ * Author: Emerson Ronald Pereira
+ *****************************************************/
+
 // Atualizar ano atual
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 
