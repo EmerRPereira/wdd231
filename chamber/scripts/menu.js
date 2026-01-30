762ce76bc44e560fb437b9ee61cc3fa4171b1054
EmerRPereira <104523253+EmerRPereira@users.noreply.github.com>
2026-01-30 10:28:20 -0300

    Update menu.js

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,3 +1,10 @@
+/*****************************************************
+ * scripts/menu.js
+ * WDD231 – Chamber Website
+ * Mobile Menu Toggle
+ * Author: Emerson Ronald Pereira
+ *****************************************************/
+
 const menuButton = document.querySelector("#menu");
 const nav = document.querySelector("nav");
 
