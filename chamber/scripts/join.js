78ab0a768f601dd1daa8dbff82e181a2aeaea8ed
EmerRPereira <104523253+EmerRPereira@users.noreply.github.com>
2026-01-30 10:28:46 -0300

    Create join.js

---
+++ chamber/scripts/join.js
@@ -0,0 +1,10 @@
+/*****************************************************
+ * scripts/join.js
+ * WDD231 – Chamber Join Page
+ * Membership Application Form Script
+ * Author: Emerson Ronald Pereira
+ *****************************************************/
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("timestamp").value = new Date().toLocaleString();
+});
