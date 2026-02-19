5bfeb58e601a82978a6c97887a4bf6e580bd3624
EmerRPereira <104523253+EmerRPereira@users.noreply.github.com>
2026-02-19 01:31:06 -0300

    Update navigation.js

--- finalproject/scripts/navigation.js
+++ finalproject/scripts/navigation.js
@@ -12,5 +12,8 @@ const nav = document.querySelector("#navigation");
 if (button && nav) {
   button.addEventListener("click", () => {
     nav.classList.toggle("open");
+
+    const isOpen = nav.classList.contains("open");
+    button.setAttribute("aria-expanded", isOpen);
   });
 }
