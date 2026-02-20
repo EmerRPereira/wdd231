8487e55afba77de5a4b51aa575bdf5defcd564de
EmerRPereira <104523253+EmerRPereira@users.noreply.github.com>
2026-02-20 01:09:20 -0300

    Update storege.js

--- finalproject/scripts/storege.js
+++ finalproject/scripts/storege.js
@@ -6,6 +6,11 @@
  * Location: Curitiba, Brazil
  *****************************************************/
 
+const savedCart = JSON.parse(localStorage.getItem("cart")) || [];
+cart = savedCart;
+
+localStorage.setItem("cart", JSON.stringify(cart));
+
 export function saveToStorage(key,value){
 localStorage.setItem(key,JSON.stringify(value));
 }
@@ -13,3 +18,9 @@ localStorage.setItem(key,JSON.stringify(value));
 export function getFromStorage(key){
 return JSON.parse(localStorage.getItem(key));
 }
+
+let orders = JSON.parse(localStorage.getItem("orders")) || [];
+
+orders.push(newOrder);
+
+localStorage.setItem("orders", JSON.stringify(orders));
