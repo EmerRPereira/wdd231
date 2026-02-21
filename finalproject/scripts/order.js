86672bf9f49da64823ac4ee5580f36e8a9cec63b
EmerRPereira <104523253+EmerRPereira@users.noreply.github.com>
2026-02-21 04:47:39 -0300

    Update order.js

--- finalproject/scripts/order.js
+++ finalproject/scripts/order.js
@@ -41,7 +41,7 @@ renderToppings();
 /* ===== LOAD PRODUCTS ===== */
 async function loadProducts() {
   try {
-  const response = await fetch("https://github.com/EmerRPereira/wdd231/finalproject/data/products.json");
+  const response = await fetch("https://emerrpereira.github.io/wdd231/finalproject/data/products.json");
 
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
